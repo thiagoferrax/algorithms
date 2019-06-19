15602bdbd351a315335cd3f408dcfc4b60cf689b
thiagoferrax <thiagoferrax@gmail.com>
2019-06-18 19:40:22 -0500

    MissingInteger - Code review and improvements.

--- src/main/java/com/trainings/algorithms/countingelements/MissingInteger.java
+++ src/main/java/com/trainings/algorithms/countingelements/MissingInteger.java
@@ -13,16 +13,18 @@ public class MissingInteger {
 		Arrays.sort(A);
 
 		int N = A.length;
-		if (N == 0 || A[0] > 1 || A[N - 1] < 0) {
+		if (N == 0 || A[0] > 1 || A[N - 1] <= 0) {
 			return 1;
 		}
 
-		for (int n = 1; n < N; n++) {
+		int n = avoidNegativeIntegers(A, N);
+		
+		for (; n < N; n++) {
 
 			if (A[n] < 0)
 				continue;
 
-			int before = A[n - 1] < 0 ? 0 : A[n - 1];
+			int before = A[n - 1] <= 0 ? 0 : A[n - 1];
 
 			if (A[n] - before > 1) {
 				return before + 1;
@@ -30,5 +32,16 @@ public class MissingInteger {
 		}
 
 		return A[N - 1] + 1;
+	}
+
+	private int avoidNegativeIntegers(int[] A, int N) {
+		int n = 1;
+		int half = Math.max(n, N/2);
+
+		while(half < N && A[half] <= 0) {
+			n = half+1;
+			half += (N-half)/2;
+		}
+		return n;
 	}
 }
--- src/test/java/com/trainings/algorithms/countingelements/MissingIntegerTest.java
+++ src/test/java/com/trainings/algorithms/countingelements/MissingIntegerTest.java
@@ -14,6 +14,8 @@ class MissingIntegerTest {
 		Assert.assertEquals(5, missingInteger.solution(new int[] {1, 3, 6, 4, 1, 2}));
 		Assert.assertEquals(1, missingInteger.solution(new int[] {2}));
 		Assert.assertEquals(1, missingInteger.solution(new int[] {4, 5, 6, 2}));
+		Assert.assertEquals(1, missingInteger.solution(new int[] {-1, -3, -2, 0}));
+		Assert.assertEquals(3, missingInteger.solution(new int[] {-1, -4, -3, -2, -2, -5, 5, 0, 1, 2, 7, 8}));
 	}
 
 }
