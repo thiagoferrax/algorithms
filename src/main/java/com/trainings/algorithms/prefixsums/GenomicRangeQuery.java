900c75331890065b68f08f87c7d6d719e5daa60f
thiagoferrax <thiagoferrax@gmail.com>
2019-06-20 10:26:49 -0300

    GenomicRangeQuery - Code review and improvements.

--- src/main/java/com/trainings/algorithms/prefixsums/GenomicRangeQuery.java
+++ src/main/java/com/trainings/algorithms/prefixsums/GenomicRangeQuery.java
@@ -1,55 +1,35 @@
 package com.trainings.algorithms.prefixsums;
 
+import java.util.Arrays;
+
 /**
  * Find the minimal nucleotide from a range of sequence DNA.
  * https://app.codility.com/programmers/lessons/5-prefix_sums/genomic_range_query/
  */
 public class GenomicRangeQuery {
-	
-	//Detected time complexity: O(N * M)
+
+	// Detected time complexity: O(N * M)
 	public int[] solution(String S, int[] P, int[] Q) {
 		int M = P.length;
 
 		int[] aSolution = new int[M];
 		for (int m = 0; m < M; m++) {
-			char[] range = S.substring(P[m], Q[m] + 1).toCharArray();
+			String range = S.substring(P[m], Q[m] + 1);
 			aSolution[m] = getMinimalImpact(range);
 		}
 
 		return aSolution;
 	}
 
-	private int getMinimalImpact(char[] range) {
-		int N = range.length;
+	private int getMinimalImpact(String range) {
+		char[] sequence = replaceSequence(range).toCharArray();
 
-		int minimalImpact = 4;
-		for (int n = 0; n < N; n++) {
-			int impact = getImpact(range, n);
-			minimalImpact = Math.min(minimalImpact, impact);
-		}
+		Arrays.sort(sequence);
 
-		return minimalImpact;
+		return Character.getNumericValue(sequence[0]);
 	}
 
-	private int getImpact(char[] range, int n) {
-		int impact;
-		switch (range[n]) {
-		case 'A':
-			impact = 1;
-			break;
-		case 'C':
-			impact = 2;
-			break;
-		case 'G':
-			impact = 3;
-			break;
-		case 'T':
-			impact = 4;
-			break;
-		default:
-			impact = 4;
-			break;
-		}
-		return impact;
+	public String replaceSequence(String dnaSequence) {
+		return dnaSequence.replaceAll("A+", "1").replaceAll("C+", "2").replaceAll("G+", "3").replaceAll("T+", "4");
 	}
 }
--- src/test/java/com/trainings/algorithms/prefixsums/GenomicRangeQueryTest.java
+++ src/test/java/com/trainings/algorithms/prefixsums/GenomicRangeQueryTest.java
@@ -10,5 +10,11 @@ class GenomicRangeQueryTest {
 		GenomicRangeQuery genomicRangeQuery = new GenomicRangeQuery();
 		Assert.assertArrayEquals(new int[] {2, 4, 1}, genomicRangeQuery.solution("CAGCCTA", new int[] {2, 5, 0}, new int[] {4, 5, 6}));
 	}
+	
+	@Test
+	void testReplaceSequence() {
+		GenomicRangeQuery genomicRangeQuery = new GenomicRangeQuery();
+		Assert.assertEquals("1213241", genomicRangeQuery.replaceSequence("AAAACAAAAAGCCTAA"));
+	}
 
 }
