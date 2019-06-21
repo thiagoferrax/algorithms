2bfcfc4dfb3302253a2e1908849d2fa99597ee8e
thiagoferrax <thiagoferrax@gmail.com>
2019-06-21 13:41:41 -0300

    GenomicRangeQuery - Adding a better solution but still O(NxM).

--- src/main/java/com/trainings/algorithms/prefixsums/GenomicRangeQuery.java
+++ src/main/java/com/trainings/algorithms/prefixsums/GenomicRangeQuery.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 public class GenomicRangeQuery {
 
 	// Detected time complexity: O(N * M)
-	public int[] solution(String S, int[] P, int[] Q) {
+	public int[] firstSolution(String S, int[] P, int[] Q) {
 		int M = P.length;
 
 		int[] aSolution = new int[M];
@@ -19,6 +19,39 @@ public class GenomicRangeQuery {
 		}
 
 		return aSolution;
+	}
+
+	// Detected time complexity: O(N * M)
+	public int[] solution(String S, int[] P, int[] Q) {
+		int M = P.length;
+
+		int[] aSolution = new int[M];
+		char[] sequence = S.toCharArray();
+		for (int m = 0; m < M; m++) {
+			aSolution[m] = 4;
+			for (int n = P[m]; n <= Q[m]; n++) {
+				aSolution[m] = Math.min(aSolution[m], getValue(sequence[n]));
+				if (aSolution[m] == 1) {
+					break;
+				}
+			}
+		}
+
+		return aSolution;
+	}
+
+	private int getValue(char c) {
+		int value = 4;
+
+		if (c == 'A') {
+			value = 1;
+		} else if (c == 'C') {
+			value = 2;
+		} else if (c == 'G') {
+			value = 3;
+		}
+
+		return value;
 	}
 
 	private int getMinimalImpact(String range) {
--- src/test/java/com/trainings/algorithms/prefixsums/GenomicRangeQueryTest.java
+++ src/test/java/com/trainings/algorithms/prefixsums/GenomicRangeQueryTest.java
@@ -12,6 +12,12 @@ class GenomicRangeQueryTest {
 	}
 	
 	@Test
+	void testFirstSolution() {
+		GenomicRangeQuery genomicRangeQuery = new GenomicRangeQuery();
+		Assert.assertArrayEquals(new int[] {2, 4, 1}, genomicRangeQuery.firstSolution("CAGCCTA", new int[] {2, 5, 0}, new int[] {4, 5, 6}));
+	}
+	
+	@Test
 	void testReplaceSequence() {
 		GenomicRangeQuery genomicRangeQuery = new GenomicRangeQuery();
 		Assert.assertEquals("1213241", genomicRangeQuery.replaceSequence("AAAACAAAAAGCCTAA"));
