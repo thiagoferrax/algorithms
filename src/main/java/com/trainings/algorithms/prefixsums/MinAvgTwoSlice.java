fde56ee36630c5843b6eaf953c01e3ddbb630837
thiagoferrax <thiagoferrax@gmail.com>
2019-06-24 11:27:46 -0300

    MinAvgTwoSlice - Code review and improvements.

--- src/main/java/com/trainings/algorithms/prefixsums/MinAvgTwoSlice.java
+++ src/main/java/com/trainings/algorithms/prefixsums/MinAvgTwoSlice.java
@@ -8,23 +8,21 @@ public class MinAvgTwoSlice {
 	// O(N ** 2)
 	public int solution(int[] A) {
 		int N = A.length;
-
-		int[] sum = new int[N];
 		
+		int[] sum = new int[N];
 		sum[0] = A[0];
-		for (int n = 1; n < N; n++) {
-			sum[n] = sum[n-1] + A[n];
-		}
 
 		int aSolution = 0;
 		float minSlice = 100000 * 10000;
+		
 		for (int n = 0; n < N - 1; n++) {
 			for (int m = n + 1; m < N; m++) {
-				float sum_N_M = n >= 1 ? sum[m] - sum[n - 1] : sum[m];
-				float slice_N_M = sum_N_M / (m - n + 1);
+				sum[m] = sum[n] + A[m];
 				
-				if (slice_N_M < minSlice) {
-					minSlice = slice_N_M;
+				float sliceNM = (n >= 1 ? sum[m] - sum[n - 1] : sum[m]) / (m - n + 1);
+				
+				if (sliceNM < minSlice) {
+					minSlice = sliceNM;
 					aSolution = n;
 				}
 			}
--- src/test/java/com/trainings/algorithms/prefixsums/MinAvgTwoSliceTest.java
+++ src/test/java/com/trainings/algorithms/prefixsums/MinAvgTwoSliceTest.java
@@ -10,7 +10,8 @@ class MinAvgTwoSliceTest {
 	void test() {
 		MinAvgTwoSlice minAvgTwoSlice = new MinAvgTwoSlice();
 		
-		Assert.assertEquals(1, minAvgTwoSlice.solution(new int[] {4, 2, 2, 5, 1, 5, 8}));		
+		Assert.assertEquals(1, minAvgTwoSlice.solution(new int[] {4, 2, 2, 5, 1, 5, 8}));			
+		Assert.assertEquals(0, minAvgTwoSlice.solution(new int[] {2, 3, 7, 1}));
 	}
 
 }
