b877fa7a0a9578eebbca53cc2414a92013eeb9eb
thiagoferrax <thiagoferrax@gmail.com>
2019-06-25 12:45:59 -0300

    Distinct - Compute number of distinct values in an array.

---
+++ src/main/java/com/trainings/algorithms/sorting/Distinct.java
@@ -0,0 +1,31 @@
+package com.trainings.algorithms.sorting;
+
+import java.util.Arrays;
+
+/**
+ * Compute number of distinct values in an array.
+ * https://app.codility.com/programmers/lessons/6-sorting/distinct/ 
+ * Detected time complexity: O(N*log(N)) or O(N)
+ */
+public class Distinct {
+	public int solution(int[] A) {
+		int N = A.length;
+
+		Arrays.sort(A);
+
+		if (N == 0 || N == 1 || (N == 2 && A[0] != A[1])) {
+			return N;
+		} else if (A[0] == A[N - 1]) {
+			return 1;
+		}
+
+		int count = 1;
+		for (int i = 1; i < N; i++) {
+			if (A[i] != A[i - 1]) {
+				count++;
+			}
+		}
+
+		return count;
+	}
+}
