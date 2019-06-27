8a126fdd6c3fe403dff366d4de7121f88971c1c1
thiagoferrax <thiagoferrax@gmail.com>
2019-06-27 08:06:44 -0300

    Fish - Code review and improvements, now it is O(N).

--- src/main/java/com/trainings/algorithms/stacksandqueues/Fish.java
+++ src/main/java/com/trainings/algorithms/stacksandqueues/Fish.java
@@ -6,28 +6,57 @@ import java.util.Deque;
 /**
  * N voracious fish are moving along a river. Calculate how many fish are alive.
  * https://app.codility.com/programmers/lessons/7-stacks_and_queues/fish/
+ * Detected time complexity: O(N)
  */
 public class Fish {
+	private static final int FLOWING_UPSTREAM = 0;
+	private static final int FLOWING_DOWNSTREAM = 1;
+
+	private static final int SIZE = 0;
+	private static final int FLOW = 1;
+
 	public int solution(int[] A, int[] B) {
 		int N = A.length;
 
-		Deque<Integer> stack = new ArrayDeque<>();
+		Deque<Integer[]> alive = new ArrayDeque<>();
 
 		int currentFlow = B[0];
-		for (int b = 0; b < N; b++) {
-			int fishFlow = B[b];
-			int fishSize = A[b];
-			
-			if (currentFlow <= fishFlow) {
-				stack.push(fishSize);
-				currentFlow = fishFlow;
-			} else if (!stack.isEmpty() && stack.peek() < fishSize) {
-				stack.pop();
-				stack.push(fishSize);
-				currentFlow = fishFlow;
+		for (int n = 0; n < N; n++) {
+			int nextSize = A[n];
+			int nextFlow = B[n];
+
+			if (fishesMeetEachOther(currentFlow, nextFlow)) {
+				boolean pushNextFish = true;
+
+				while (!alive.isEmpty() && fishesMeetEachOther(alive.peek()[FLOW], nextFlow) && pushNextFish) {
+					if (alive.peek()[SIZE] < nextSize) {
+						// Current fish is eaten because it is smaller than the next one.
+						alive.pop();
+						pushNextFish = true;
+					} else if (alive.peek()[SIZE] > nextSize) {
+						/*
+						 * Current fish is bigger than the next one. There is no need to push a dead
+						 * fish in the alive stack.
+						 */
+						currentFlow = alive.peek()[FLOW];
+						pushNextFish = false;
+					}
+				}
+
+				if (pushNextFish) {
+					alive.push(new Integer[] { nextSize, nextFlow });
+					currentFlow = nextFlow;
+				}
+			} else {
+				alive.push(new Integer[] { nextSize, nextFlow });
+				currentFlow = nextFlow;
 			}
 		}
 
-		return stack.size();
+		return alive.size();
+	}
+
+	private boolean fishesMeetEachOther(int currentFlow, int nextFlow) {
+		return currentFlow == FLOWING_DOWNSTREAM && nextFlow == FLOWING_UPSTREAM;
 	}
 }
--- src/test/java/com/trainings/algorithms/stacksandqueues/FishTest.java
+++ src/test/java/com/trainings/algorithms/stacksandqueues/FishTest.java
@@ -17,7 +17,6 @@ class FishTest {
 		Assert.assertEquals(2, fish.solution(new int[] { 3, 1 }, new int[] { 0, 1 }));
 
 		Assert.assertEquals(2, fish.solution(new int[] { 4, 3, 2, 1, 5 }, new int[] { 0, 1, 0, 0, 0 }));
-		Assert.assertEquals(4, fish.solution(new int[] { 4, 3, 2, 6, 5 }, new int[] { 0, 1, 1, 0, 0 }));
 		Assert.assertEquals(5, fish.solution(new int[] { 4, 3, 2, 6, 5 }, new int[] { 1, 1, 1, 1, 1 }));
 		Assert.assertEquals(5, fish.solution(new int[] { 4, 3, 2, 6, 5 }, new int[] { 0, 0, 0, 0, 0 }));
 		Assert.assertEquals(3, fish.solution(new int[] { 4, 7, 2, 6, 5 }, new int[] { 1, 0, 1, 0, 0 }));
@@ -25,6 +24,10 @@ class FishTest {
 		Assert.assertEquals(3, fish.solution(new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 0, 1, 0, 0 }));
 		Assert.assertEquals(8, fish.solution(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new int[] { 1, 0, 0, 0, 0, 0, 0, 0, 0 }));
 		Assert.assertEquals(9, fish.solution(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new int[] { 0, 1, 1, 1, 1, 1, 1, 1, 1 }));
+		
+		Assert.assertEquals(1, fish.solution(new int[] { 5, 3, 4 }, new int[] { 1, 1, 0 }));
+		Assert.assertEquals(3, fish.solution(new int[] { 4, 3, 2, 6, 5 }, new int[] { 0, 1, 1, 0, 0 }));
+
 	}
 
 }
