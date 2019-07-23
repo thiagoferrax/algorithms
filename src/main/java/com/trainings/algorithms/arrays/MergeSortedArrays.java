c8e310d398f794888f71d7ee65019ec9bb8ddf8b
thiagoferrax <thiagoferrax@gmail.com>
2019-07-23 14:38:27 -0300

    MergeSortedArrays - Code review and improvements.

--- src/main/java/com/trainings/algorithms/arrays/MergeSortedArrays.java
+++ src/main/java/com/trainings/algorithms/arrays/MergeSortedArrays.java
@@ -2,36 +2,37 @@ package com.trainings.algorithms.arrays;
 
 /**
  * Merge two sorted arrays of integers.
+ * Time complexity: O(m+n)
  * https://www.udemy.com/master-the-coding-interview-data-structures-algorithms/learn/lecture/12309362#overview
  */
 public class MergeSortedArrays {
 	public int[] merge(int[] left, int[] right) {
-		int[] solution = new int[left.length + right.length];
+		int m = left.length;
+		int n = right.length;
+		
+		int[] solution = new int[m + n];
 
-		if (left.length == 0) {
+		if (m == 0) {
 			return right;
 		} 
-		if (right.length == 0) {
+		if (n == 0) {
 			return left;
 		}
 		
-		int l = 0, r = 0, s = 0; 
-		
-		while(s < solution.length) {
-			if(l == left.length) {
+		for (int s = 0, l = 0, r = 0; s < solution.length; s++) {
+			if(l == m) {
 				solution[s] = right[r];
 				r++;
-			} else if(r == right.length) {
+			} else if(r == n) {
 				solution[s] = left[l];
 				l++;
 			} else if(left[l] <= right[r]) {
 				solution[s] = left[l];
 				l++;
-			} else if (right[r] < left[l]) {
+			} else {
 				solution[s] = right[r];
 				r++;
 			}
-			s++;	
 		}
 		
 		return solution;
