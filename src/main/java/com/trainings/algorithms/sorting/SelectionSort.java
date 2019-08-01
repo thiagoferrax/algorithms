6563a33830173cba7a17a8298629ed43ef5d6bce
thiagoferrax <thiagoferrax@gmail.com>
2019-08-01 19:48:52 -0300

    SelectionSort - Code review and improvements.

--- src/main/java/com/trainings/algorithms/sorting/SelectionSort.java
+++ src/main/java/com/trainings/algorithms/sorting/SelectionSort.java
@@ -4,25 +4,29 @@ public class SelectionSort {
 
 	public static int[] sort(int[] values) {
 		int n = values.length;
-		if(n < 2) {
+		if (n < 2) {
 			return values;
 		}
-		
-		for(int i = 0; i < n; i++) {
-			for(int j = i+1; j < n; j++) {
-				if(values[i] > values[j]) {
-					swap(values, i, j);
+
+		for (int i = 0; i < n; i++) {
+			int smaller_position = i;
+			for (int j = i + 1; j < n; j++) {
+				if (values[smaller_position] > values[j]) {
+					smaller_position = j;
 				}
 			}
+			swap(values, i, smaller_position);
 		}
-				
+
 		return values;
 	}
 
 	private static void swap(int[] values, int i, int j) {
-		int temp = values[i];
-		values[i] = values[j];
-		values[j] = temp;
+		if (i != j) {
+			int temp = values[i];
+			values[i] = values[j];
+			values[j] = temp;
+		}
 	}
 
 }
