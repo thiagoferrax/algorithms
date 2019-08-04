f7819967a5229599b2c5cb3779fb2c15e4ed6f9e
thiagoferrax <thiagoferrax@gmail.com>
2019-08-03 22:05:06 -0300

    QuickSort - Implementing the quick sort algorithm.

---
+++ src/main/java/com/trainings/algorithms/sorting/QuickSort.java
@@ -0,0 +1,74 @@
+package com.trainings.algorithms.sorting;
+
+
+/**
+ * 
+ */
+public class QuickSort {
+
+	public static int[] sort(int[] values) {
+		return quickSort(values, 0, values.length - 1);
+	}
+
+	private static int[] quickSort(int[] values, int start, int end) {
+		int length = end - start + 1;
+
+		if (length < 2) {
+			return values;
+		}
+
+		if (length == 2) {
+			if (values[start] > values[end]) {
+				swap(values, start, end);
+			}
+		}
+
+		int selected = start;
+		int compareTo = end;
+		boolean left = true;
+		
+		while (selected != compareTo) {
+			if (left) {
+				if (values[selected] > values[compareTo]) {
+					swap(values, selected, compareTo);
+				
+					int temp = selected;
+					selected = compareTo;
+					compareTo = temp + 1;
+					left = false;
+				} else {
+					compareTo--;
+				}
+			} else {
+				if (values[selected] < values[compareTo]) {
+					swap(values, selected, compareTo);
+					
+					int temp = selected;
+					selected = compareTo;
+					compareTo = temp - 1;
+					left = true;
+				} else {
+					compareTo++;
+				}
+			}
+		}
+		
+		if(selected == start) {
+			quickSort(values, selected + 1, end);
+		} else if(selected == end) {
+			quickSort(values, start, end - 1);
+		} else {
+			quickSort(values, start, selected-1);
+			quickSort(values, selected + 1, end);
+		}
+
+		return values;
+	}
+
+	private static void swap(int[] values, int i, int j) {
+		int temp = values[i];
+		values[i] = values[j];
+		values[j] = temp;
+	}
+
+}
---
+++ src/test/java/com/trainings/algorithms/sorting/QuickSortTest.java
@@ -0,0 +1,18 @@
+package com.trainings.algorithms.sorting;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class QuickSortTest {
+
+	@Test
+	void test() {
+		Assert.assertArrayEquals(new int[] {}, QuickSort.sort(new int[] {}));
+		Assert.assertArrayEquals(new int[] {1}, QuickSort.sort(new int[] {1}));
+		Assert.assertArrayEquals(new int[] {1, 2}, QuickSort.sort(new int[] {2, 1}));
+		Assert.assertArrayEquals(new int[] {1, 2, 3}, QuickSort.sort(new int[] {3, 2, 1}));
+		Assert.assertArrayEquals(new int[] {1, 2, 3, 4}, QuickSort.sort(new int[] {4, 3, 2, 1}));
+		Assert.assertArrayEquals(new int[] {1, 2, 3, 4, 5, 6, 6, 7}, QuickSort.sort(new int[] {7, 4, 3, 5, 6, 6, 2, 1}));
+	}    
+
+}
