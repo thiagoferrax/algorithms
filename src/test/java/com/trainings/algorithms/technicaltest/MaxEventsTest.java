a9c9cfbd30dee8456e35607a5cb43b42950a1890
thiago.cunha <thiago.cunha@dataprev.gov.br>
2019-08-13 08:04:02 -0300

    MaxEvents - Code review and improvements.

--- src/test/java/com/trainings/algorithms/technicaltest/MaxEventsTest.java
+++ src/test/java/com/trainings/algorithms/technicaltest/MaxEventsTest.java
@@ -13,6 +13,7 @@ class MaxEventsTest {
 		Assert.assertEquals(3, ResultMaxEvents.maxEvents(Arrays.asList(1, 1, 1, 1, 5), Arrays.asList(10, 3, 1, 4, 2)));
 		Assert.assertEquals(4, ResultMaxEvents.maxEvents(Arrays.asList(1, 1, 1, 1), Arrays.asList(10, 3, 1, 4)));
 		Assert.assertEquals(2, ResultMaxEvents.maxEvents(Arrays.asList(1, 3, 5, 7), Arrays.asList(12, 2, 2, 2)));
+		Assert.assertEquals(4, ResultMaxEvents.maxEvents(Arrays.asList(1, 3, 5, 7), Arrays.asList(1, 3, 1, 4)));
 	}
 
 }
