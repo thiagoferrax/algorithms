cc4be38828f4621b4b109958be53ae01fb1dbb55
thiago.cunha <thiago.cunha@dataprev.gov.br>
2019-08-23 17:20:41 -0300

    ArrayManipulation - Code review and improvements.

--- src/main/java/com/trainings/algorithms/arrays/ArrayManipulation.java
+++ src/main/java/com/trainings/algorithms/arrays/ArrayManipulation.java
@@ -3,6 +3,10 @@ package com.trainings.algorithms.arrays;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 /**
@@ -14,12 +18,48 @@ import java.util.Scanner;
  * @author thiago
  *
  */
+
+class Interval {
+	private Integer start;
+	private Integer end;
+	private Integer maxValue;
+
+	public Interval(int start, int end, int maxValue) {
+		super();
+		this.start = start;
+		this.end = end;
+		this.maxValue = maxValue;
+	}
+
+	public Integer getMaxValue() {
+		return maxValue;
+	}
+
+	public void setMaxValue(int maxValue) {
+		this.maxValue = maxValue;
+	}
+
+	public Integer getStart() {
+		return start;
+	}
+
+	public Integer getEnd() {
+		return end;
+	}
+
+	@Override
+	public String toString() {
+		return "start=" + start + ", end=" + end + ", maxValue=" + maxValue;
+	}
+
+}
+
 public class ArrayManipulation {
 	private static final int VALUE_TO_ADD = 2;
 	private static final int END = 1;
 	private static final int START = 0;
 
-	static long arrayManipulation(int n, int[][] queries) {
+	public static long arrayManipulationInitialVersion(int n, int[][] queries) {
 
 		long[] values = new long[n];
 
@@ -42,6 +82,92 @@ public class ArrayManipulation {
 
 		return max;
 
+	}
+
+	public static long arrayManipulation(int n, int[][] queries) {
+
+		if (queries.length == 1) {
+			return queries[0][VALUE_TO_ADD];
+		}
+
+		long max = 0;
+
+		PriorityQueue<Interval> intervals = new PriorityQueue<Interval>(new Comparator<Interval>() {
+			@Override
+			public int compare(Interval o1, Interval o2) {
+				return o1.getStart().compareTo(o2.getStart());
+			}
+		});
+		intervals.add(buildInterval(queries, 0));
+
+		for (int q = 1; q < queries.length; q++) {
+			Interval current = buildInterval(queries, q);
+
+			List<Interval> mergedIntervals = merge(current, intervals.poll());
+			intervals.addAll(mergedIntervals);
+
+		}
+
+		for (Interval interval : intervals) {
+			max = Math.max(max, interval.getMaxValue());
+		}
+
+		return max;
+
+	}
+
+	private static List<Interval> merge(Interval interval1, Interval interval2) {
+		List<Interval> mergedIntervals = new ArrayList<>();
+
+		Integer start1 = interval1.getStart();
+		Integer end1 = interval1.getEnd();
+
+		Integer start2 = interval2.getStart();
+		Integer end2 = interval2.getEnd();
+
+		if (start1 < start2) {
+			int end = Math.min(end1, start2) - 1;
+			mergedIntervals.add(new Interval(start1, end, interval1.getMaxValue()));
+
+			if (start2 <= end1) {
+				int endMin = Math.min(end1, end2);
+				int endMax = Math.max(end1, end2);
+
+				mergedIntervals.add(new Interval(start2, endMin, interval1.getMaxValue() + interval2.getMaxValue()));
+				mergedIntervals.add(new Interval(endMin + 1, endMax, interval2.getMaxValue()));
+			} else if (end1 < start2) {
+				mergedIntervals.add(new Interval(start2, end2, interval2.getMaxValue()));
+			}
+
+		} else if (start1 > start2) {
+			int end = Math.min(end2, start1) - 1;
+			mergedIntervals.add(new Interval(start2, end, interval2.getMaxValue()));
+
+			if (start1 <= end2) {
+				int endMin = Math.min(end1, end2);
+				int endMax = Math.max(end1, end2);
+
+				mergedIntervals.add(new Interval(start1, endMin, interval1.getMaxValue() + interval2.getMaxValue()));
+				mergedIntervals.add(new Interval(endMin + 1, endMax, interval1.getMaxValue()));
+			} else if (end2 < start1) {
+				mergedIntervals.add(new Interval(start1, end1, interval1.getMaxValue()));
+			}
+		} else {
+			int end = Math.min(end1, end2);
+			mergedIntervals.add(new Interval(start2, end, interval1.getMaxValue() + interval2.getMaxValue()));
+
+			if (end1 <= end2) {
+				mergedIntervals.add(new Interval(end1 + 1, end2, interval2.getMaxValue()));
+			} else {
+				mergedIntervals.add(new Interval(end2 + 1, end1, interval1.getMaxValue()));
+			}
+		}
+
+		return mergedIntervals;
+	}
+
+	private static Interval buildInterval(int[][] queries, int i) {
+		return new Interval(queries[i][START], queries[i][END], queries[i][VALUE_TO_ADD]);
 	}
 
 	private static final Scanner scanner = new Scanner(System.in);
--- src/test/java/com/trainings/algorithms/arrays/ArrayManipulationTest.java
+++ src/test/java/com/trainings/algorithms/arrays/ArrayManipulationTest.java
@@ -9,6 +9,13 @@ class ArrayManipulationTest {
 	void test() {
 		Assert.assertEquals(200L,
 				ArrayManipulation.arrayManipulation(5, new int[][] { { 1, 2, 100 }, { 2, 5, 100 }, { 3, 4, 100 } }));
+		Assert.assertEquals(70L,
+				ArrayManipulation.arrayManipulation(4, new int[][] { { 1, 2, 50 }, { 3, 4, 10 }, { 1, 4, 20 } }));
+		Assert.assertEquals(250L,
+				ArrayManipulation.arrayManipulation(5, new int[][] { { 1, 2, 240 }, { 3, 4, -20 }, { 3, 5, 250 } }));
+		
+//		Assert.assertEquals(31L,
+//				ArrayManipulation.arrayManipulation(10, new int[][] { { 2, 6, 8 }, { 3, 5, 7 }, { 1, 8, 1 }, { 5, 9, 15 } }));
 	}
 
 }
