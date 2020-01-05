db2fbf5b652008bead0f41eca58da44196df0604
thiago.cunha <thiago.cunha@dataprev.gov>
2020-01-05 11:55:02 -0300

    Comparator - Create a comparator and use it to sort an array.

---
+++ src/test/java/com/trainings/algorithms/sorting/Comparator.java
@@ -0,0 +1,59 @@
+package com.trainings.algorithms.sorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Player {	
+	String name;
+	int score;
+
+	Player(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+
+	@Override
+	public String toString() {
+		return "Player [name=" + name + ", score=" + score + "]";
+	}
+	
+	
+}
+
+class Checker implements java.util.Comparator<Player> {
+	// complete this method
+	public int compare(Player a, Player b) {
+		int comparison = Integer.valueOf(b.score).compareTo(a.score);
+		
+		if(comparison == 0) {
+			comparison = a.name.compareTo(b.name);
+		}
+		
+		return comparison;
+	}
+}
+
+/**
+ * https://www.hackerrank.com/challenges/ctci-comparator-sorting
+ * Create a comparator and use it to sort an array.
+ */
+public class Comparator {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+
+		Player[] player = new Player[n];
+		Checker checker = new Checker();
+
+		for (int i = 0; i < n; i++) {
+			player[i] = new Player(scan.next(), scan.nextInt());
+		}
+		scan.close();
+
+		Arrays.sort(player, checker);
+		for (int i = 0; i < player.length; i++) {
+			System.out.printf("%s %s\n", player[i].name, player[i].score);
+		}
+	}
+}
---
+++ src/test/java/com/trainings/algorithms/sorting/ComparatorTest.java
@@ -0,0 +1,33 @@
+package com.trainings.algorithms.sorting;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class ComparatorTest {
+
+	@Test
+	void test() {
+		Player amy = new Player("amy", 100);
+		Player david = new Player("david", 100);
+		Player heraldo = new Player("heraldo", 50);
+		Player aakansha = new Player("aakansha", 75);
+		Player aleksa = new Player("aleksa", 150);
+
+		List<Player> players = new ArrayList<Player>();
+		players.add(amy);
+		players.add(david);
+		players.add(heraldo);
+		players.add(aakansha);
+		players.add(aleksa);
+		
+		Collections.sort(players, new Checker());
+		
+		Assert.assertArrayEquals(Arrays.asList(aleksa, amy, david, aakansha, heraldo).toArray(), players.toArray());
+	}
+
+}
