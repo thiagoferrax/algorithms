c063260c5aaa183f978d3409646cdbdc2f2c24c3
thiagoferrax <thiagoferrax@gmail.com>
2020-06-20 18:24:12 +0100

    Code review and fixes.

--- src/test/java/com/trainings/algorithms/https/TransactionsByLocationAndIdTest.java
+++ src/test/java/com/trainings/algorithms/https/TransactionsByLocationAndIdTest.java
@@ -7,15 +7,28 @@ import org.junit.jupiter.api.Test;
 class TransactionsByLocationAndIdTest {
 
 	@Test
-	void test() {
-
-		int userId = 2;
-		int locationId = 8;
-		int netStart = 119;
-		int netEnd = 212;
-		
-		Assert.assertEquals(-6473, Result.getTransactions(userId, locationId, netStart, netEnd));
+	void test1() {
+		Assert.assertEquals(-6474, Result.getTransactions(2, 8, 119, 212));
 
 	}
 
+	@Test
+	void test2() {
+		Assert.assertEquals(2549, Result.getTransactions(4, 6, 100, 250));
+	}
+
+	@Test
+	void test3() {
+		Assert.assertEquals(0, Result.getTransactions(1, 9, 25, 75));
+	}
+	
+	@Test
+	void test4() {
+		Assert.assertEquals(0, Result.getTransactions(2, 8, 5, 50));
+	}
+	
+	@Test
+	void test5() {
+		Assert.assertEquals(-1101, Result.getTransactions(1, 9, 100, 220));
+	}
 }
