70504c7b7b5cdf2dd33c016eec6c7d178e1d4b09
thiagoferrax <thiagoferrax@gmail.com>
2020-06-28 20:50:07 +0000

    Code review and adjustments.

--- src/main/java/com/trainings/algorithms/strings/MakingAnagrams.java
+++ src/main/java/com/trainings/algorithms/strings/MakingAnagrams.java
@@ -3,7 +3,6 @@ package com.trainings.algorithms.strings;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.Scanner;
@@ -11,6 +10,7 @@ import java.util.Scanner;
 /**
  * MakingAnagrams - return an integer representing the minimum total characters
  * that must be deleted to make the strings anagrams.
+ * 
  * https://www.hackerrank.com/challenges/ctci-making-anagrams/
  * 
  * @author thiago
@@ -24,37 +24,39 @@ public class MakingAnagrams {
 		char[] charsA = a.toCharArray();
 		char[] charsB = b.toCharArray();
 
-		Map<Character, Integer> mapA = getCharTimesMap(charsA);
-		Map<Character, Integer> mapB = getCharTimesMap(charsB);
+		Map<Character, Integer> mapA = getCharQuantityMap(charsA);
+		Map<Character, Integer> mapB = getCharQuantityMap(charsB);
 
 		int minRemovals = 0;
+		
 		for (char charA : mapA.keySet()) {
+			Integer quantityA = mapA.get(charA);
 			if (mapB.containsKey(charA)) {
-				minRemovals += Math.abs(mapA.get(charA) - mapB.get(charA));
+				minRemovals += Math.abs(quantityA - mapB.get(charA));
 			} else {
-				minRemovals+=mapA.get(charA);
+				minRemovals += quantityA;
 			}
 		}
 
 		for (char charB : mapB.keySet()) {
 			if (!mapA.containsKey(charB)) {
-				minRemovals+=mapB.get(charB);
+				minRemovals += mapB.get(charB);
 			}
 		}
+		
 		return minRemovals;
-
 	}
 
-	private static Map<Character, Integer> getCharTimesMap(char[] charsA) {
+	private static Map<Character, Integer> getCharQuantityMap(char[] charsA) {
 		Map<Character, Integer> mapA = new Hashtable<>();
 		for (int i = 0; i < charsA.length; i++) {
-			Integer times = mapA.get(charsA[i]);
-			if (times == null) {
-				times = 0;
-				mapA.put(charsA[i], times);
+			Integer quantity = mapA.get(charsA[i]);
+			if (quantity == null) {
+				quantity = 0;
+				mapA.put(charsA[i], quantity);
 			}
 
-			mapA.put(charsA[i], ++times);
+			mapA.put(charsA[i], ++quantity);
 		}
 		return mapA;
 	}
