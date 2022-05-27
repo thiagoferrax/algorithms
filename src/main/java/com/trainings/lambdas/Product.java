4540dc5ad2f25a18c19bb565bc1e058f91979557
Thiago Cunha <thiagoferrax@gmail.com>
2022-05-27 20:21:43 -0300

    Lambdas - Calculating product end price with lambdas.

---
+++ src/main/java/com/trainings/lambdas/Product.java
@@ -0,0 +1,28 @@
+package com.trainings.lambdas;
+
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+public class Product {
+	private String name;
+	private Double price;
+	private Double discount;
+
+	public Product(String name, Double price, Double discount) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.discount = discount;
+	}
+
+	public static BinaryOperator<Double> priceWithDiscount = (price, discount) -> price * (1 - discount);
+	public static UnaryOperator<Double> tax = price -> price >= 2500 ? price * (1 + 0.085) : price;
+	public static UnaryOperator<Double> delivery = price -> price >= 3000 ? price + 100 : price + 50;
+	public static Function<Double, String> format = price -> String.format("R$%.2f", price);
+
+	public String getTotalPrice() {
+		return priceWithDiscount.andThen(tax).andThen(delivery).andThen(format).apply(price, discount);
+	}
+
+}
---
+++ src/test/java/com/trainings/lambdas/ProductTest.java
@@ -0,0 +1,14 @@
+package com.trainings.lambdas;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ProductTest {
+
+	@Test
+	public void testLabdas() {
+		Product product = new Product("iPad", 3235.89, 0.13);
+		Assert.assertEquals("R$3154,52", product.getTotalPrice());
+	}
+
+}
