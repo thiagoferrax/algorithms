2c704d200020a0cce6941f342cd0e27a48552363
Thiago Cunha <thiagoferrax@gmail.com>
2022-06-15 13:44:20 -0300

    Knapsack - Adding a bottom up solution for the Knapsack.

--- src/main/java/com/trainings/algorithms/dynamicprogramming/Knapsack.java
+++ src/main/java/com/trainings/algorithms/dynamicprogramming/Knapsack.java
@@ -36,4 +36,43 @@ public class Knapsack {
 		return max;
 	}
 
+	public int solveKnapsackBottomUpSolution(int[] profits, int[] weights, int capacity) {
+		// Basic checks
+		int n = profits.length;
+		if (capacity <= 0 || n == 0 || weights.length == 0 || n != weights.length) {
+			return 0;
+		}
+
+		int[][] dp = new int[n][capacity + 1];
+
+		// Populate the capacity 0 as profit 0
+		for (int i = 0; i < n; i++) {
+			dp[i][0] = 0;
+		}
+
+		// Populate for the first weight its profit if that weight is less than or
+		// equals the current capacity.
+		for (int c = 1; c <= capacity; c++) {
+			if (weights[0] <= c) {
+				dp[0][c] = profits[0];
+			}
+		}
+
+		for (int i = 1; i < n; i++) {
+			for (int c = 0; c <= capacity; c++) {
+				int profit1 = 0, profit2 = 0;
+				
+				if(weights[i] <= c) {
+					profit1 = profits[i] + dp[i-1][c - weights[i]];
+				}
+				
+				profit2 = dp[i-1][c];
+				
+				dp[i][c] = Math.max(profit1, profit2);
+			}
+		}
+
+		return dp[n-1][capacity];
+	}
+
 }
--- src/main/java/com/trainings/algorithms/dynamicprogramming/KnapsackTest.java
+++ src/main/java/com/trainings/algorithms/dynamicprogramming/KnapsackTest.java
@@ -15,5 +15,16 @@ class KnapsackTest {
 		assertEquals(22, ks.solveKnapsack(profits, weights, 7));
 		assertEquals(17, ks.solveKnapsack(profits, weights, 6));
 	}
+	
+	@Test
+	void testBottomUpSolution() {
+		Knapsack ks = new Knapsack();
+		int[] profits = { 1, 6, 10, 16 };
+		int[] weights = { 1, 2, 3, 5 };
+
+		assertEquals(22, ks.solveKnapsackBottomUpSolution(profits, weights, 7));
+		assertEquals(17, ks.solveKnapsackBottomUpSolution(profits, weights, 6));
+	}
+
 
 }
