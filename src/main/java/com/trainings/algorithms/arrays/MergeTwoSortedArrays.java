ebe17fae3eb0d8ef7d49a98e05f19f54360b0d49
Thiago Cunha <thiagoferrax@gmail.com>
2022-06-15 12:27:44 -0300

    MergeTwoSortedArrays - Adding more solutions now when there is a limit parameter for the merged list.

--- src/main/java/com/trainings/algorithms/arrays/MergeTwoSortedArrays.java
+++ src/main/java/com/trainings/algorithms/arrays/MergeTwoSortedArrays.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 // https://www.geeksforgeeks.org/merge-two-sorted-arrays/
 public class MergeTwoSortedArrays {
 
-	public static int[] mergeArrays(int[] arr1, int[] arr2, int n1, int n2) {
+	public static int[] mergeArrays2ndSolution(int[] arr1, int[] arr2, int n1, int n2) {
 		List<Integer> l1 = asList(arr1);
 		l1.addAll(asList(arr2));
 		Collections.sort(l1);
@@ -19,29 +19,53 @@ public class MergeTwoSortedArrays {
 		return Arrays.stream(arr1).boxed().collect(Collectors.toList());
 	}
 
-	public static int[] mergeArrays1stSolution(int[] arr1, int[] arr2, int n1, int n2) {
+	public static int[] mergeArrays(int[] arr1, int[] arr2, int n1, int n2) {
 
-		// Create the new array that is the merge between arr1 and arr2.
-		int[] merged = new int[arr1.length + arr2.length];
+		int[] merged = new int[n1 + n2];
 
-		int index1 = 0;
-		int index2 = 0;
-
-		int index = 0;
-
-		// Iterate through arrays adding values in merged according to comparisons.
-		while (index1 < arr1.length || index2 < arr2.length) {
-			if (index1 >= arr1.length) {
-				merged[index++] = arr2[index2++];
-			} else if (index2 >= arr2.length) {
-				merged[index++] = arr2[index2++];
-			} else if (arr1[index1] <= arr2[index2]) {
-				merged[index++] = arr1[index1++];
+		int m = 0, a1 = 0, a2 = 0;
+		while (a1 < n1 || a2 < n2) {
+			if (a1 >= n1) {
+				merged[m++] = arr2[a2++];
+			} else if (a2 >= n2) {
+				merged[m++] = arr1[a1++];
+			} else if (arr1[a1] <= arr2[a2]) {
+				merged[m++] = arr1[a1++];
 			} else {
-				merged[index++] = arr2[index2++];
+				merged[m++] = arr2[a2++];
 			}
 		}
 
 		return merged;
+	}
+	
+	public static int[] mergeArrays(int[] arr1, int[] arr2, int limit) {
+
+		int n1 = arr1.length;
+		int n2 = arr2.length;
+		
+		int[] merged = new int[Math.min(n1 + n2, limit)];
+
+		int m = 0, a1 = 0, a2 = 0;
+		while ((a1 < n1 || a2 < n2) && m < limit) {
+			if (a1 >= n1) {
+				merged[m++] = arr2[a2++];
+			} else if (a2 >= n2) {
+				merged[m++] = arr1[a1++];
+			} else if (arr1[a1] <= arr2[a2]) {
+				merged[m++] = arr1[a1++];
+			} else {
+				merged[m++] = arr2[a2++];
+			}
+		}
+
+		return merged;
+	}
+	
+	public static int[] mergeArrays2ndSolution(int[] arr1, int[] arr2, int limit) {
+		List<Integer> l1 = asList(arr1);
+		l1.addAll(asList(arr2));
+		Collections.sort(l1);
+		return l1.stream().limit(limit).mapToInt(i -> i).toArray();
 	}
 }
--- src/main/java/com/trainings/algorithms/arrays/MergeTwoSortedArraysTest.java
+++ src/main/java/com/trainings/algorithms/arrays/MergeTwoSortedArraysTest.java
@@ -12,4 +12,16 @@ class MergeTwoSortedArraysTest {
 				MergeTwoSortedArrays.mergeArrays(new int[] { 1, 3, 4, 5 }, new int[] { 2, 4, 6, 8 }, 4, 4));
 	}
 
+	@Test
+	void testWithLimit() {
+		assertArrayEquals(new int[] { 1, 2, 3 },
+				MergeTwoSortedArrays.mergeArrays(new int[] { 1, 3, 4, 5 }, new int[] { 2, 4, 6, 8 }, 3));
+	}
+	
+	@Test
+	void testWithLimitUsingStreams() {
+		assertArrayEquals(new int[] { 1, 2, 3 },
+				MergeTwoSortedArrays.mergeArrays2ndSolution(new int[] { 1, 3, 4, 5 }, new int[] { 2, 4, 6, 8 }, 3));
+	}
+
 }
