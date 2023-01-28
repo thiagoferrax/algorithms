81d2ed8aa4502a2c71ce5d53b73a484ecc4a7028
thiagoferrax <thiagoferrax@gmail.com>
2023-01-28 22:00:41 +0000

    Updating to use java 19 and resolving some use of deprecated methods.

--- src/main/java/com/trainings/algorithms/sorting/MedianOfTwoSortedArraysTest.java
+++ src/main/java/com/trainings/algorithms/sorting/MedianOfTwoSortedArraysTest.java
@@ -1,19 +1,19 @@
 package com.trainings.algorithms.sorting;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 class MedianOfTwoSortedArraysTest {
 
     @Test
     void testHappyDay() {
-        assertEquals(2.0, new MedianOfTwoSortedArrays().findMedianSortedArrays(new int[]{1, 3}, new int[]{2}));
+        assertEquals(2.0, new MedianOfTwoSortedArrays().findMedianSortedArrays(new int[]{1, 3}, new int[]{2}), 0);
     }
 
     @Test
     void testSecondHappyDay() {
-        assertEquals(2.5, new MedianOfTwoSortedArrays().findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}));
+        assertEquals(2.5, new MedianOfTwoSortedArrays().findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}), 0);
     }
 
 }
