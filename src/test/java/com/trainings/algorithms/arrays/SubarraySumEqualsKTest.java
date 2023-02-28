fafdbf20f60a57dbde749911d5fc746278cd0544
thiagoferrax <thiagoferrax@gmail.com>
2023-02-28 18:55:03 -0300

    Subarray Sum Equals K - Leedcode - Better algorithm but still with Time Limit Exceeded.

--- src/test/java/com/trainings/algorithms/arrays/SubarraySumEqualsKTest.java
+++ src/test/java/com/trainings/algorithms/arrays/SubarraySumEqualsKTest.java
@@ -17,5 +17,10 @@ public class SubarraySumEqualsKTest {
 	public void testTwo() {
 		assertEquals(2, new SubarraySumEqualsK().subarraySum(new int[] {1,2,3}, 3));
 	}
+	
+	@Test
+	public void testThree() {
+		assertEquals(3, new SubarraySumEqualsK().subarraySum(new int[] {1,-1,0}, 0));
+	}
 
 }
