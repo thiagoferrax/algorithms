7c2a43467f2f7cb6003943d369e2453a9da91b21
thiagoferrax <thiagoferrax@gmail.com>
2023-03-17 15:37:04 -0300

    Adding more one test case, the one showed in the Vanhack challenge.

--- src/test/java/com/trainings/algorithms/arrays/ShortestPathInAMatrixTest.java
+++ src/test/java/com/trainings/algorithms/arrays/ShortestPathInAMatrixTest.java
@@ -9,4 +9,9 @@ public class ShortestPathInAMatrixTest {
     public void testOne() {
         assertEquals("1->3->10->1", new ShortestPathInAMatrix().shortestPath(new int[][]{{1,20, 3}, {3,10, 1}}));
     }
+
+    @Test
+    public void testTwo() {
+        assertEquals("2->10->1->4->2->1->1", new ShortestPathInAMatrix().shortestPath(new int[][]{{2,10, 1, 3}, {10,5, 4, 5}, {2, 10, 2, 1}, {25, 2, 5, 1}}));
+    }
 }
