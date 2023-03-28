9fa1be424dbdb86dd543c09822ea1b351e4cc28f
thiagoferrax <thiagoferrax@gmail.com>
2023-03-28 09:46:27 -0300

    Longest valid parentheses from leetcode, not submitted yet.

---
+++ src/main/java/com/trainings/algorithms/stacksandqueues/LongestValidParentheses.java
@@ -0,0 +1,37 @@
+package com.trainings.algorithms.stacksandqueues;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class LongestValidParentheses {
+
+    public int longestValidParentheses(String s) {
+        int longest = 0;
+
+        Map<Character, Character> parenthesesMap = new HashMap<>();
+        parenthesesMap.put(')', '(');
+
+        Stack<Character> stack = new Stack<>();
+
+        int count = 0;
+        if(s != null && s.length() > 1) {
+            // find the longest valid parentheses
+            for (int i = 0; i < s.length(); i++) {
+                char character = s.charAt(i);
+                Character peek = !stack.isEmpty() ? stack.peek() : null;
+                if(peek != null && peek == parenthesesMap.get(character)) {
+                    stack.pop();
+                    count+=2;
+                    if(longest < count) {
+                        longest = count;
+                    }
+                } else {
+                    stack.push(character);
+                }
+            }
+        }
+
+        return longest;
+    }
+}
---
+++ src/test/java/com/trainings/algorithms/stacksandqueues/LongestValidParenthesesTest.java
@@ -0,0 +1,27 @@
+package com.trainings.algorithms.stacksandqueues;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * <a href="https://leetcode.com/problems/longest-valid-parentheses/">longest-valid-parentheses</a>
+ */
+public class LongestValidParenthesesTest {
+
+
+    @Test
+    public void testOne() {
+        assertEquals(2, new LongestValidParentheses().longestValidParentheses("(()"));
+    }
+
+    @Test
+    public void testTwo() {
+        assertEquals(4, new LongestValidParentheses().longestValidParentheses(")()())"));
+    }
+
+    @Test
+    public void testThree() {
+        assertEquals(0, new LongestValidParentheses().longestValidParentheses(""));
+    }
+}
