469ad9bd1d10d60e0e34469155e7922e33e33056
thiagoferrax <thiagoferrax@gmail.com>
2023-04-02 17:46:23 -0300

    SuccessfulPairsOfSpellsAndPotions from leet code, memory limit exceeded.

--- src/main/java/com/trainings/algorithms/arrays/SuccessfulPairsOfSpellsAndPotions.java
+++ src/main/java/com/trainings/algorithms/arrays/SuccessfulPairsOfSpellsAndPotions.java
@@ -1,7 +1,39 @@
 package com.trainings.algorithms.arrays;
 
+import java.util.Arrays;
+
 public class SuccessfulPairsOfSpellsAndPotions {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int[] successfulPairs = new int[spells.length];
+
+        int[] pairs = new int[spells.length * potions.length];
+        int s = 0, count = 0;
+        for (int i = 0; i < pairs.length; i++) {
+            pairs[i] = potions[i%potions.length];
+        }
+
+        System.out.println(Arrays.toString(pairs));
+
+        for (int i = 0; i < pairs.length; i++) {
+            pairs[i] *= spells[s];
+
+            if(pairs[i] >= success) {
+                count++;
+            }
+
+            if((i + 1) % potions.length == 0) {
+                successfulPairs[s] = count;
+                s++;
+                count = 0;
+            }
+        }
+
+        System.out.println(Arrays.toString(pairs));
+
+        return successfulPairs;
+    }
+
+    public int[] successfulPairs1stSolution(int[] spells, int[] potions, long success) {
         int[] successfulPairs = new int[spells.length];
 
         for (int i = 0; i < spells.length; i++) {
--- src/test/java/com/trainings/algorithms/arrays/SuccessfulPairsOfSpellsAndPotionsTest.java
+++ src/test/java/com/trainings/algorithms/arrays/SuccessfulPairsOfSpellsAndPotionsTest.java
@@ -8,7 +8,12 @@ import static org.junit.Assert.*;
 
 public class SuccessfulPairsOfSpellsAndPotionsTest {
     @Test
-    public void testOne(){
-        assertEquals(Arrays.toString(new int[]{4,0,3}), Arrays.toString(new SuccessfulPairsOfSpellsAndPotions().successfulPairs(new int[]{5,1,3}, new int[]{1,2,3,4,5}, 7)));
+    public void testOne() {
+        assertEquals(Arrays.toString(new int[]{4, 0, 3}), Arrays.toString(new SuccessfulPairsOfSpellsAndPotions().successfulPairs(new int[]{5, 1, 3}, new int[]{1, 2, 3, 4, 5}, 7)));
+    }
+
+    @Test
+    public void testTwo() {
+        assertEquals(Arrays.toString(new int[]{1, 1, 1}), Arrays.toString(new SuccessfulPairsOfSpellsAndPotions().successfulPairs(new int[]{15, 14, 39}, new int[]{22}, 224)));
     }
 }
