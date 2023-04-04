ee2ebb0cae52deacfa856c890607ab0b1924c1f8
thiagoferrax <thiagoferrax@gmail.com>
2023-04-04 09:30:44 -0300

    Count good nodes in binary tree from leet code.

---
+++ src/test/java/com/trainings/algorithms/trees/CountGoodNodesInBinaryTreeTest.java
@@ -0,0 +1,22 @@
+package com.trainings.algorithms.trees;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CountGoodNodesInBinaryTreeTest {
+
+    @Test
+    public void testOne() {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(1);
+        root.left.left = new TreeNode(3);
+        root.left.right = null;
+        root.right = new TreeNode(4);
+        root.right.left = new TreeNode(1);
+        root.right.right = new TreeNode(5);
+
+        assertEquals(4, new CountGoodNodesInBinaryTree().goodNodes(root));
+    }
+
+}
