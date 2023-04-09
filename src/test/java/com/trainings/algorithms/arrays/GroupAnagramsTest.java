d923e835db2e73627e81c3bec2d61f6f2484af1f
thiagoferrax <thiagoferrax@gmail.com>
2023-04-10 00:53:56 +0100

    Adjusting a test case.

--- src/test/java/com/trainings/algorithms/arrays/GroupAnagramsTest.java
+++ src/test/java/com/trainings/algorithms/arrays/GroupAnagramsTest.java
@@ -3,15 +3,15 @@ package com.trainings.algorithms.arrays;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class GroupAnagramsTest {
     @Test
     public void test() {
-        assertEquals(Arrays.asList(Arrays.asList("bat"),
-                        Arrays.asList("nat", "tan"),
-                        Arrays.asList("ate", "eat", "tea")),
+        assertEquals(Arrays.asList(List.of("eat", "tea", "ate"),
+                        List.of("bat"),  List.of("tan", "nat")),
                 new GroupAnagrams().groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
     }
 }
