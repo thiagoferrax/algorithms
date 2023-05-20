444143318f70351bb1de5766dc7f1236b0e4c2ce
thiagoferrax <thiagoferrax@gmail.com>
2023-05-20 20:08:48 +0100

    PalindromIndex from HackerHank - Still time limit exceeded.

--- src/main/java/com/trainings/algorithms/arrays/PalindromeIndex.java
+++ src/main/java/com/trainings/algorithms/arrays/PalindromeIndex.java
@@ -2,14 +2,19 @@ package com.trainings.algorithms.arrays;
 
 public class PalindromeIndex {
     public static int palindromeIndex(String s) {
-        System.out.println("palindromeIndex " + s);
+
+        String reverse = new StringBuilder(s).reverse().toString();
 
         //If the word is already a palindrome
         int index = -1;
-        if(!isPalindrom(s)) {
+        if(!isPalindrom(s, reverse)) {
             for (int i = 0; i < s.length(); i++) {
                 String word = getWordRemovingCharAt(s, i);
-                if(isPalindrom(word)) {
+                String reverseWord = getWordRemovingCharAt(reverse, s.length() - 1 - i);
+
+                System.out.printf("The word %s and the reverse word %s.", word, reverseWord);
+
+                if(isPalindrom(word, reverseWord)) {
                     index =  i;
                     break;
                 }
@@ -26,21 +31,23 @@ public class PalindromeIndex {
             word += s.substring(i+1);
         }
 
-        System.out.println(String.format("This is the %s when removing %d", word, i));
+        System.out.printf("This is the %s when removing %d%n", word, i);
 
         return word;
     }
 
-    private static boolean isPalindrom(String s) {
+    private static boolean isPalindrom(String s, String reverse) {
 
         int add = s.length() % 2 == 0 ? 0 : 1;
 
         String firstPart = s.substring(0, s.length()/2 + add);
         String secondPart = s.substring(s.length()/2);
+        String secondPartReverse = reverse.substring(0, s.length()/2 + add);
 
         System.out.println("firstPart: " + firstPart);
         System.out.println("secondPart: " + secondPart);
+        System.out.println("secondPartReverse: " + secondPartReverse);
 
-        return firstPart.equals(new StringBuilder(secondPart).reverse().toString());
+        return firstPart.equals(secondPartReverse);
     }
 }
